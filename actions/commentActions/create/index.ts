f8f15537ad0961c558321cc5cfa8ee6e755f59ab
thuee-mong-marma <thueemongs@gmail.com>
2024-04-17 09:00:28 +0600

    added comment model relation to card

--- actions/commentActions/create/index.ts
+++ actions/commentActions/create/index.ts
@@ -27,7 +27,6 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       data: {
         message,
         cardId,
-        boardId,
         userId,
         userImage,
         userName,
